00d25d807bfc5fe0c9270cebfb3d875868bd3619
Arkis9011 <kisileariel@gmail.com>
2023-08-07 15:02:28 +0100

    first commit

---
+++ formulaire.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+<body>
+
+<?php
+// $a = "Jenovic";
+//  echo "Bonjour $a";
+?>
+<section>
+<div class="container mt-5">
+<form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Nom</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">Vas y hunnn écrit</div>
+  </div>
+  <div class="mb-3">
+  
+</form>
+</section>
+
+<button type="button" class="btn btn-primary"><a href="index.php">Liste</a></button>
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+<body>
+
+<?php
+// $a = "Jenovic";
+//  echo "Bonjour $a";
+?>
+<section>
+<div class="container mt-5">
+<ul class="list-group">
+  <li class="list-group-item">An item</li>
+  <li class="list-group-item">A second item</li>
+  <li class="list-group-item">A third item</li>
+  <li class="list-group-item">A fourth item</li>
+  <li class="list-group-item">And a fifth one</li>
+</ul>
+</div>
+</section>
+<button type="button" class="btn btn-primary"><a href="formulaire.php">Formulaire</a></button>
+<section>
+
+</section>
+
+
+</body>
+</html>
